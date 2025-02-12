2e8f93f822b3712dea57dc1a9c789cadef42c23b
Steven Jacovitch <sjacovit@umich.edu>
2025-02-12 13:40:27 -0500

    Created a 'Next' button that allows for navigation between onboarding screens

--- screens/PersonalInformationScreen.js
+++ screens/PersonalInformationScreen.js
@@ -1,12 +1,18 @@
 // New User Onboarding Screen for Personal Information
 
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 
-const PersonalInformationScreen = () => {
+const PersonalInformationScreen = ({ navigation }) => {
     return (
         <View style={styles.container}>
-            <Text>Personal Information Screen</Text>
+            <Text style={styles.text}>Personal Information Screen</Text>
+            <TouchableOpacity 
+                style={styles.button} 
+                onPress={() => navigation.navigate('CommunicationStyleScreen')}
+            >
+                <Text style={styles.buttonText}>Next</Text>
+            </TouchableOpacity>
         </View>
     );
 };
@@ -17,5 +23,21 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
+    text: {
+        fontFamily: 'Muller, Sans-serif',
+    },
+    button: {
+        marginTop: 20,
+        padding: 10,
+        backgroundColor: '#1EA896',
+        borderRadius: 50,
+        width: '80%',
+        alignItems: 'center',
+    },
+    buttonText: {
+        color: '#FFFFFF',
+        fontSize: 16,
+    },
 });
+
 export default PersonalInformationScreen;
