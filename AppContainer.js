b90560d1da8b680256e692103bd66adee1f5c0b8
Dhwani Rayathatha <dhwanirayathatha@Dhwanis-MacBook-Pro.local>
2025-02-20 02:09:30 -0500

    SCRUM-8-User-Login-Screen

---
+++ AppContainer.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import LoginScreen from './screens/LoginScreen';
+
+const Stack = createNativeStackNavigator();
+
+const AppContainer = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator 
+        initialRouteName="Login"
+        screenOptions={{
+          headerShown: false
+        }}
+      >
+        <Stack.Screen name="Login" component={LoginScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default AppContainer;
